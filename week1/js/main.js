70e89b478d4bf7955e0ce3dbb67c7fa967d832d0
Melanie Monkurai <Mon17045@byui.edu>
2022-01-08 21:56:41 -0700

    initial commit

--- week1/js/main.js
+++ week1/js/main.js
@@ -10,5 +10,6 @@ function loadStory(){
   };
   function displayStory(){
     var storyHTML = document.getElementById("story_editor").value;
+    
     document.getElementById("story_display").innerHTML = storyHTML;
   };
